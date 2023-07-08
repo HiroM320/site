fcfb7ed5260677a5cb176a969aeabdbf47eb996b
HiroM320 <56062913+HiroM320@users.noreply.github.com>
2023-07-09 03:04:58 +0900

    Formatter(Prettier) をセットアップ

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Options} */
+module.exports = {
+  plugins: [require.resolve('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
